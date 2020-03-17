1d362b22441fcaf1f8ae6e9f648545fce093e10c
dfounderliu <dfounderliu@tencent.com>
2020-03-17 12:14:03 +0800

    增加多地域部署能力

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 const ensureIterable = require('type/iterable/ensure')
-const ensurePlainObject = require('type/plain-object/ensure')
-const ensureString = require('type/string/ensure')
-const random = require('ext/string/random')
 const path = require('path')
 const { Component, utils } = require('@serverless/core')
 
@@ -19,129 +16,47 @@ class TencentFlask extends Component {
     return 'http'
   }
 
-  /**
-   * prepare create function inputs
-   * @param {object} inputs inputs
-   */
-  async prepareInputs(inputs = {}) {
-    inputs.name =
-      ensureString(inputs.functionName, { isOptional: true }) ||
-      this.state.functionName ||
-      `FlaskComponent_${random({ length: 6 })}`
-    inputs.codeUri = ensureString(inputs.code, { isOptional: true }) || process.cwd()
-    inputs.region = ensureString(inputs.region, { default: 'ap-guangzhou' })
-    inputs.include = ensureIterable(inputs.include, { default: [], ensureItem: ensureString })
-    inputs.exclude = ensureIterable(inputs.exclude, { default: [], ensureItem: ensureString })
-    inputs.apigatewayConf = ensurePlainObject(inputs.apigatewayConf, { default: {} })
-
-    const shimsDir = path.join(__dirname, 'shims')
-    inputs.include = [
-      path.join(shimsDir, 'severless_wsgi.py'),
-      path.join(shimsDir, 'api_service.py')
-    ]
-    inputs.exclude.push('.git/**', '.gitignore', '.serverless', '.DS_Store')
-
-    inputs.handler = ensureString(inputs.handler, { default: DEFAULTS.handler })
-    inputs.runtime = ensureString(inputs.runtime, { default: DEFAULTS.runtime })
-    inputs.apigatewayConf = ensurePlainObject(inputs.apigatewayConf, { default: {} })
-
-    const appFile = path.join(path.resolve(inputs.codeUri), 'app.py')
-    if (!(await utils.fileExists(appFile))) {
-      throw new Error(`app.py not found in ${inputs.codeUri}`)
-    }
-
-    if (inputs.functionConf) {
-      inputs.timeout = inputs.functionConf.timeout ? inputs.functionConf.timeout : 3
-      inputs.memorySize = inputs.functionConf.memorySize ? inputs.functionConf.memorySize : 128
-      if (inputs.functionConf.environment) {
-        inputs.environment = inputs.functionConf.environment
-      }
-      if (inputs.functionConf.vpcConfig) {
-        inputs.vpcConfig = inputs.functionConf.vpcConfig
-      }
-    }
-
-    inputs.requirements = ensurePlainObject(inputs.apigatewayConf, { default: {} })
-    inputs.requirements.include = inputs.include
-    inputs.requirements.runtime = inputs.runtime.toLowerCase()
-    inputs.requirements.codeUri = inputs.codeUri
-    if (!inputs.requirements.dockerizePip) {
-      inputs.requirements.pythonBin = 'python'
-    }
-
-    return inputs
-  }
-
   async default(inputs = {}) {
-    inputs = await this.prepareInputs(inputs)
+    const appFile = path.join(path.resolve(inputs.code), 'app.py')
+    if (!(await utils.fileExists(appFile))) {
+      throw new Error(`app.py not found in ${inputs.code}`)
+    }
 
     const pyRequirements = await this.load('@yugasun/python-requirements', 'pyRequirements')
     const pyRequirementsOutput = await pyRequirements(inputs.requirements)
+    const shimsDir = path.join(__dirname, 'shims')
+    inputs.include = ensureIterable(inputs.include, { default: pyRequirementsOutput.include })
+    inputs.include.push(path.join(shimsDir, 'severless_wsgi.py'))
+    inputs.include.push(path.join(shimsDir, 'api_service.py'))
+    inputs.exclude = ensureIterable(inputs.exclude, { default: [] })
+    inputs.exclude.push('.git/**', '.gitignore', '.serverless', '.DS_Store')
+    inputs.handler = DEFAULTS.handler
+    inputs.runtime = DEFAULTS.runtime
 
-    inputs.include = pyRequirementsOutput.include
+    const Framework = await this.load('@serverless/tencent-framework')
 
-    const tencentCloudFunction = await this.load('@serverless/tencent-scf')
-    const tencentApiGateway = await this.load('@serverless/tencent-apigateway')
+    const framworkOutpus = await Framework({
+      ...inputs,
+      ...{
+        framework: 'flask'
+      }
+    })
 
-    inputs.fromClientRemark = inputs.fromClientRemark || 'tencent-flask'
-    const tencentCloudFunctionOutputs = await tencentCloudFunction(inputs)
-    const apigwParam = {
-      serviceName: inputs.serviceName,
-      description: 'Serverless Framework tencent-flask Component',
-      serviceId: inputs.serviceId,
-      region: inputs.region,
-      protocols: inputs.apigatewayConf.protocols || ['http'],
-      environment:
-        inputs.apigatewayConf && inputs.apigatewayConf.environment
-          ? inputs.apigatewayConf.environment
-          : 'release',
-      endpoints: [
-        {
-          path: '/',
-          method: 'ANY',
-          function: {
-            isIntegratedResponse: true,
-            functionName: tencentCloudFunctionOutputs.Name
-          }
-        }
-      ]
-    }
-
-    if (inputs.apigatewayConf && inputs.apigatewayConf.auth) {
-      apigwParam.endpoints[0].usagePlan = inputs.apigatewayConf.usagePlan
-    }
-    if (inputs.apigatewayConf && inputs.apigatewayConf.auth) {
-      apigwParam.endpoints[0].auth = inputs.apigatewayConf.auth
-    }
-
-    apigwParam.fromClientRemark = inputs.fromClientRemark || 'tencent-flask'
-    const tencentApiGatewayOutputs = await tencentApiGateway(apigwParam)
-    const outputs = {
-      region: inputs.region,
-      functionName: inputs.name,
-      apiGatewayServiceId: tencentApiGatewayOutputs.serviceId,
-      url: `${this.getDefaultProtocol(tencentApiGatewayOutputs.protocols)}://${
-        tencentApiGatewayOutputs.subDomain
-      }/${tencentApiGatewayOutputs.environment}/`
-    }
-
-    this.state = outputs
-
+    this.state = framworkOutpus
     await this.save()
-
-    return outputs
+    return framworkOutpus
   }
 
   async remove(inputs = {}) {
-    const removeInput = {
-      fromClientRemark: inputs.fromClientRemark || 'tencent-flask'
-    }
-    const tencentCloudFunction = await this.load('@serverless/tencent-scf')
-    const tencentApiGateway = await this.load('@serverless/tencent-apigateway')
-
-    await tencentCloudFunction.remove(removeInput)
-    await tencentApiGateway.remove(removeInput)
-
+    const Framework = await this.load('@serverless/tencent-framework')
+    await Framework.remove({
+      ...inputs,
+      ...{
+        framework: 'flask'
+      }
+    })
+    this.state = {}
+    await this.save()
     return {}
   }
 }
