9e1977a55c4b2f4ca5f1ba3f56bf0b27e65c5a07
Avril Li <linxforever@vip.qq.com>
2020-07-01 14:57:55 +0800

    chore: update test case

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,7 +1,7 @@
 const { generateId, getServerlessSdk } = require('./utils')
 
 // set enough timeout for deployment to finish
-jest.setTimeout(30000)
+jest.setTimeout(300000)
 
 // the yaml file we're testing against
 const instanceYaml = {
@@ -11,8 +11,9 @@ const instanceYaml = {
   name: `flask-integration-tests-${generateId()}`,
   stage: 'dev',
   inputs: {
-    runtime: 'Python3.6'
-    // region: 'ap-guangzhou'
+    runtime: 'Python3.6',
+    region: 'ap-guangzhou',
+    apigatewayConf: { environment: 'test' }
   }
 }
 
@@ -24,53 +25,28 @@ const credentials = {
 // get serverless construct sdk
 const sdk = getServerlessSdk(instanceYaml.org)
 
-// clean up the instance after tests
-afterAll(async () => {
-  await sdk.remove(instanceYaml, credentials)
-})
-
 it('should successfully deploy flask app', async () => {
   const instance = await sdk.deploy(instanceYaml, { tencent: {} })
   expect(instance).toBeDefined()
   expect(instance.instanceName).toEqual(instanceYaml.name)
   // get src from template by default
   expect(instance.outputs.templateUrl).toBeDefined()
-})
-
-it('should successfully update basic configuration', async () => {
-  instanceYaml.inputs.region = 'ap-shanghai'
-  instanceYaml.inputs.functionName = 'flaskDemoTest'
-
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
   expect(instance.outputs).toBeDefined()
   expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
   expect(instance.outputs.scf).toBeDefined()
   expect(instance.outputs.scf.runtime).toEqual(instanceYaml.inputs.runtime)
-  expect(instance.outputs.scf.functionName).toEqual(instanceYaml.inputs.functionName)
-})
-
-it('should successfully update apigatewayConf', async () => {
-  instanceYaml.inputs.apigatewayConf = { environment: 'test' }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
   expect(instance.outputs.apigw).toBeDefined()
   expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
-
-})
-
-it('should successfully disable auto create api gateway', async () => {
-  instanceYaml.inputs.apigatewayConf = { isDisabled: true }
-  const instance = await sdk.deploy(instanceYaml, credentials)
-
-  expect(instance.outputs).toBeDefined()
-  expect(instance.outputs.apigw).not.toBeDefined()
 })
 
 it('should successfully remove flask app', async () => {
   await sdk.remove(instanceYaml, credentials)
-  result = await sdk.getInstance(instanceYaml.org, instanceYaml.stage, instanceYaml.app, instanceYaml.name)
+  result = await sdk.getInstance(
+    instanceYaml.org,
+    instanceYaml.stage,
+    instanceYaml.app,
+    instanceYaml.name
+  )
 
   // remove action won't delete the service cause the apigw have the api binded
   expect(result.instance.instanceStatus).toEqual('inactive')
