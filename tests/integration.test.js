31bc27157b315bb69ccc3f0b5fe2ee5a752ef30a
Avril Li <linxforever@vip.qq.com>
2020-07-03 11:45:16 +0800

    Add integration test (#11)
    * chore(test): add integration test and update isDisabled config
    
    * chore: update jest version to support node8.x
    
    * chore: remove process.env variables of test
    
    * chore: update test case

---
+++ tests/integration.test.js
@@ -0,0 +1,53 @@
+const { generateId, getServerlessSdk } = require('./utils')
+
+// set enough timeout for deployment to finish
+jest.setTimeout(300000)
+
+// the yaml file we're testing against
+const instanceYaml = {
+  org: 'orgDemo',
+  app: 'appDemo',
+  component: 'flask@dev',
+  name: `flask-integration-tests-${generateId()}`,
+  stage: 'dev',
+  inputs: {
+    runtime: 'Python3.6',
+    region: 'ap-guangzhou',
+    apigatewayConf: { environment: 'test' }
+  }
+}
+
+// get credentials from process.env but need to init empty credentials object
+const credentials = {
+  tencent: {}
+}
+
+// get serverless construct sdk
+const sdk = getServerlessSdk(instanceYaml.org)
+
+it('should successfully deploy flask app', async () => {
+  const instance = await sdk.deploy(instanceYaml, { tencent: {} })
+  expect(instance).toBeDefined()
+  expect(instance.instanceName).toEqual(instanceYaml.name)
+  // get src from template by default
+  expect(instance.outputs.templateUrl).toBeDefined()
+  expect(instance.outputs).toBeDefined()
+  expect(instance.outputs.region).toEqual(instanceYaml.inputs.region)
+  expect(instance.outputs.scf).toBeDefined()
+  expect(instance.outputs.scf.runtime).toEqual(instanceYaml.inputs.runtime)
+  expect(instance.outputs.apigw).toBeDefined()
+  expect(instance.outputs.apigw.environment).toEqual(instanceYaml.inputs.apigatewayConf.environment)
+})
+
+it('should successfully remove flask app', async () => {
+  await sdk.remove(instanceYaml, credentials)
+  result = await sdk.getInstance(
+    instanceYaml.org,
+    instanceYaml.stage,
+    instanceYaml.app,
+    instanceYaml.name
+  )
+
+  // remove action won't delete the service cause the apigw have the api binded
+  expect(result.instance.instanceStatus).toEqual('inactive')
+})
